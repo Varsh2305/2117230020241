1ef808d10f39efa4ded28b470ec3422e7c1fa94e
Varsh2305 <varshravi23@gmail.com>
2026-05-06 12:57:16 +0530

    commit

--- notification_app_fe/campus_notification_platform/src/app/ThemeRegistry.js
+++ notification_app_fe/campus_notification_platform/src/app/ThemeRegistry.js
@@ -1,5 +1,10 @@
 "use client";
+
+import { useState } from "react";
+import { useServerInsertedHTML } from "next/navigation";
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
 
 const theme = createTheme({
   typography: {
@@ -11,10 +16,48 @@ const theme = createTheme({
 });
 
 export default function ThemeRegistry({ children }) {
+  const [{ cache, flush }] = useState(() => {
+    const cache = createCache({ key: "mui" });
+    cache.compat = true;
+    const prevInsert = cache.insert;
+    let inserted = [];
+    cache.insert = (...args) => {
+      const serialized = args[1];
+      if (cache.inserted[serialized.name] === undefined) {
+        inserted.push(serialized.name);
+      }
+      return prevInsert(...args);
+    };
+    const flush = () => {
+      const prevInserted = inserted;
+      inserted = [];
+      return prevInserted;
+    };
+    return { cache, flush };
+  });
+
+  useServerInsertedHTML(() => {
+    const names = flush();
+    if (names.length === 0) return null;
+    let styles = "";
+    for (const name of names) {
+      styles += cache.inserted[name];
+    }
+    return (
+      <style
+        key={cache.key}
+        data-emotion={`${cache.key} ${names.join(" ")}`}
+        dangerouslySetInnerHTML={{ __html: styles }}
+      />
+    );
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
